12bb2f3a122a312109eac61a4bc4b86f8e921504
Jordan Burgess <jordanburgess@gmail.com>
2013-04-30 16:33:14 +0100

    settings modified

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -154,3 +154,10 @@ LOGGING = {
         },
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
