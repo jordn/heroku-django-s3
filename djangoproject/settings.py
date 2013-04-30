4d0236fe9ad4ec7a4c604f0567ef09a62ff81742
Jordan Burgess <jordanburgess@gmail.com>
2013-04-30 22:04:53 +0100

    added the heroku subdomain to allowed host

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -34,7 +34,8 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+# MUST SET TO DOMAIN ON HEROKU                                                <--------!!!!!
+ALLOWED_HOSTS = ['fathomless-ravine-5669.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
