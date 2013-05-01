e62bdbc6cd93de3feac72dd1731b66362c0b1731
Jordan Burgess <jordanburgess@gmail.com>
2013-05-01 17:12:14 +0100

    added topy

---
+++ topy/README.md
@@ -0,0 +1,2 @@
+Linked from site-packages directory.
+ln -s /Users/jordanburgess/Projects/topology-optimisation/topy /Users/jordanburgess/.virtualenvs/topology-optimisation/lib/python2.7/site-packages/topy
---
+++ topy/__init__.py
@@ -0,0 +1,13 @@
+"""
+# ==============================================================================
+# ToPy -- Topology optimisation with Python.
+# Copyright (C) 2012, William Hunter <willemjagter@gmail.com>
+# ==============================================================================
+"""
+
+from core.topology import *
+from core.visualisation import *
+from core.elements import *
+
+__version__ = "0.1.1"
+__author__  = "William Hunter <willemjagter@gmail.com>"
