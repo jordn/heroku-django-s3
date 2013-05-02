b6128f1601051dd35d62b8bbf44f35193d7d696c
Jordan Burgess <jordanburgess@gmail.com>
2013-05-02 15:42:17 +0100

    added localhost 127.0.0.1 so will run locally with DEBUG=False

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -35,7 +35,7 @@ DATABASES = {
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 # SET TO THE SUBDOMAIN ON HEROKU, ANYWHERE ELSE IT'S HOSTED (INSECURE PRESENTLY)    <--------!!!!!
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
